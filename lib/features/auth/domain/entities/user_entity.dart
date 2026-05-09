1d103a150f749b98244ead10ea4e1958900d85d4
JeivCH <jvrfck@gmail.com>
2026-05-09 22:17:51 +0200

    feat: add UserEntity domain entity

---
+++ lib/features/auth/domain/entities/user_entity.dart
@@ -0,0 +1,22 @@
+import 'package:equatable/equatable.dart';
+
+class UserEntity extends Equatable {
+  final String id;
+  final String? fullName;
+  final String email;
+  final String role;
+  final String? avatarUrl;
+  final DateTime createdAt;
+
+  const UserEntity(
+      {required this.id,
+      this.fullName,
+      required this.email,
+      required this.role,
+      this.avatarUrl,
+      required this.createdAt});
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [id, fullName, email, role, avatarUrl, createdAt];
+}
