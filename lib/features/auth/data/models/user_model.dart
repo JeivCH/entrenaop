1a3ccae10b657a726cccde5a3c1059781506b697
JeivCH <jvrfck@gmail.com>
2026-05-10 11:05:13 +0200

    feat: add UserModel with fromJson and toJson

---
+++ lib/features/auth/data/models/user_model.dart
@@ -0,0 +1,32 @@
+import 'package:entrenaop/features/auth/domain/entities/user_entity.dart';
+
+class UserModel extends UserEntity {
+  const UserModel(
+      {required super.id,
+      required super.email,
+      required super.role,
+      required super.createdAt,
+      super.fullName,
+      super.avatarUrl});
+
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    return UserModel(
+        id: json['id'],
+        email: json['email'],
+        role: json['role'],
+        createdAt: DateTime.parse(json['created_at']),
+        fullName: json['full_name'],
+        avatarUrl: json['avatar_url']);
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'email': email,
+      'role': role,
+      'created_at': createdAt.toIso8601String(),
+      'full_name': fullName,
+      'avatar_url': avatarUrl
+    };
+  }
+}
