30944ec8185cf8b2b573a77c7f30d67ec95ad54a
JeivCH <jvrfck@gmail.com>
2026-05-10 10:47:12 +0200

    feat: add auth use cases

---
+++ lib/features/auth/domain/usecases/get_current_user_usecase.dart
@@ -0,0 +1,12 @@
+import 'package:entrenaop/features/auth/domain/entities/user_entity.dart';
+import 'package:entrenaop/features/auth/domain/repositories/auth_repository.dart';
+
+class GetCurrentUserUseCase {
+  final AuthRepository repository;
+
+  GetCurrentUserUseCase(this.repository);
+
+  Future<UserEntity?> call() async {
+    return await repository.getCurrentUser();
+  }
+}
