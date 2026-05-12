14976ea7dcbfbd0dc9be0aac72080c04ea2e4498
JeivCH <jvrfck@gmail.com>
2026-05-12 23:14:38 +0200

    feat: exercise repository_impl complete

---
+++ lib/features/exercises/data/repositories/exercise_repository_impl.dart
@@ -0,0 +1,43 @@
+import 'package:entrenaop/features/exercises/data/datasources/exercise_remote_datasource.dart';
+import 'package:entrenaop/features/exercises/data/models/exercise_model.dart';
+import 'package:entrenaop/features/exercises/domain/entities/exercise_entity.dart';
+import 'package:entrenaop/features/exercises/domain/repositories/exercise_repository.dart';
+
+class ExerciseRepositoryImpl implements ExerciseRepository {
+  final ExerciseRemoteDataSource remoteDataSource;
+
+  ExerciseRepositoryImpl({required this.remoteDataSource});
+
+  @override
+  Future<void> createExercise(ExerciseEntity exercise) async {
+    return await remoteDataSource.createExercise(exercise as ExerciseModel);
+  }
+
+  @override
+  Future<void> deleteExercise(String id) async {
+    return await remoteDataSource.deleteExercise(id);
+  }
+
+  @override
+  Future<ExerciseEntity?> getExerciseById(String id) async {
+    return await remoteDataSource.getExerciseById(id);
+  }
+
+  @override
+  Future<List<ExerciseEntity>> getExercises() async {
+    return await remoteDataSource.getExercises();
+  }
+
+  @override
+  Future<List<ExerciseEntity>> getExercisesByMuscleGroup(
+      String muscleGroup) async {
+    return await remoteDataSource.getExercisesByMuscleGroup(muscleGroup);
+  }
+
+  @override
+  Future<void> updateExercise(ExerciseEntity exercise) async {
+    return await remoteDataSource.updateExercise(exercise as ExerciseModel);
+  }
+
+  // Implement the methods and properties defined in ExerciseRepository
+}
